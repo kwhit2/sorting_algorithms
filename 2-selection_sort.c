db7faed532a27be25a6aba685f8a3718ed076f72
kwhit2 <kfw2@outlook.com>
2021-02-01 16:35:00 +0000

    Output Fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,25 +11,26 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min, temp;
+	size_t i, j, min;
+	int temp;
 
-	if (array == NULL)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
 		}
-/* if (min == 1) */
-/* continue; */
-	temp = array[min];
-	array[min] = array[i];
-	array[i] = temp;
-	print_array(array, size);
+		if (i != min)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
 	}
 }
